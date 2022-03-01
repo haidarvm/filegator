3ea8aef571e4d2f5c8379d7f787bb8a35c4b50bb
haidarvm <haidarvm@gmail.com>
2022-03-01 21:42:12 +0700

    office viewer using microsoft

--- frontend/views/partials/Office.vue
+++ frontend/views/partials/Office.vue
@@ -36,6 +36,7 @@ export default {
     googleDocs(path) {
       const officePhp = this.getOfficePhp(path)
       console.log('office path nya ='+ officePhp)
+      // return 'ttps://docs.google.com/viewerng/viewer?url=https://play.hyd-ant.app/invoice.docx&embedded=true'
       return 'https://view.officeapps.live.com/op/embed.aspx?src=' + officePhp
     },
     download(path) {
