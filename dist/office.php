c53ccbef141b1f4b47017fcafb98f164a353b5ac
haidarvm <haidarvm@gmail.com>
2022-03-01 23:59:30 +0700

    repair office read dir path

--- dist/office.php
+++ dist/office.php
@@ -1,7 +1,10 @@
 <?php
 $path = base64_decode($_GET['path']);
 $rpath = str_replace("/", '', $path);
-header('Content-Disposition: attachement;filename="'.$rpath.'"');
+$full_path = dirname(__DIR__).'/repository'.$path;
+$filename =  basename($path);       
+// echo $filename;exit;
+header('Content-Disposition: attachement;filename="'.$filename.'"');
 header('Content-Transfer-Encoding: binary');
 // readfile('/home/haidar/public_html/test/php/filegatorm/repository'.$path);
-readfile(dirname(__FILE__).'/../repository'.$path);
+// readfile($full_path);
