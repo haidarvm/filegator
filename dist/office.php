bea2463f2e9ab1c8623b7f5ef5c350640e945707
haidarvm <haidarvm@gmail.com>
2022-03-01 18:46:36 +0700

    get  office

--- dist/office.php
+++ dist/office.php
@@ -4,4 +4,4 @@ $rpath = str_replace("/", '', $path);
 header('Content-Disposition: attachement;filename="'.$rpath.'"');
 header('Content-Transfer-Encoding: binary');
 // readfile('/home/haidar/public_html/test/php/filegatorm/repository'.$path);
-readfile(dirname(__FILE__).'../repository'.$path);
+readfile(dirname(__FILE__).'/../repository'.$path);
